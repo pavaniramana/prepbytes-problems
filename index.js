424d902f5479bb7f247452378ac123a7459e1e80
pavaniramana <pavanikatasala@gmail.com>
2022-05-05 12:54:35 +0530

    first

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("Hello");
+// console.log("Hello");
 
 // function sum (a,b){
 //     let add = a+b
@@ -32,10 +32,35 @@ console.log("Hello");
 // }
 // conditioncheck(12,20)
 
-function lengthnum (a,b,c) {
-    const d = a+b+c
-    console.log(d);
-    const e = (d/300)*100
-    console.log(e);
+// function lengthnum (a,b,c) {
+//     const d = a+b+c
+//     console.log(d);
+//     const e = (d/300)*100
+//     console.log(e);
+// }
+// lengthnum(50,20,100)
+
+
+// function calculator  (a,b) {
+//     switch (calculator) {
+//         case '+':
+//             console.log(a+b)
+//             break;
+    
+//         default:
+//             // console.log("hello");
+//             break;
+//     }
+// }
+// calculator(1,2)
+
+
+function checkdivisiblity(a) {
+    if(a%6==0 &&a%9==0 ) {
+        console.log("divisible by two");
+    }
+    else{
+        console.log("not divisible by two");
+    }
 }
-lengthnum(50,20,100)
+checkdivisiblity(36)
